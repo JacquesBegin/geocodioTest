2c5961ca3c241baa86afcc5352f4b4a6eaba0f93
Jacques Begin <jacques.andre.begin@gmail.com>
2019-12-22 23:54:24 -0500

    Adds example strings for request queries.

--- geocoding/apiQueries.js
+++ geocoding/apiQueries.js
@@ -1,5 +1,12 @@
+// Example queries:
+// https://api.geocod.io/v1.4/geocode?q=1109+N+Highland+St%2c+Arlington+VA&api_key=YOUR_API_KEY
+// https://api.geocod.io/v1.4/geocode?street=1109+N+Highland+St&city=Arlington&state=VA&api_key=YOUR_API_KEY
+
 
 let singleQueryURL = "https://api.geocod.io/v1.4/geocode";
+
+
+
 let street = "";
 let city = "";
 let state = "";
