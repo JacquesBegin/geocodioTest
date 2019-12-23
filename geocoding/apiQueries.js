4f641fd4838d5a9be1eb9e1fd07b142e74d46830
Jacques Begin <jacques.andre.begin@gmail.com>
2019-12-22 23:55:16 -0500

    Adds require dotenv.

--- geocoding/apiQueries.js
+++ geocoding/apiQueries.js
@@ -2,6 +2,8 @@
 // https://api.geocod.io/v1.4/geocode?q=1109+N+Highland+St%2c+Arlington+VA&api_key=YOUR_API_KEY
 // https://api.geocod.io/v1.4/geocode?street=1109+N+Highland+St&city=Arlington&state=VA&api_key=YOUR_API_KEY
 
+require('dotenv').config()
+
 
 let singleQueryURL = "https://api.geocod.io/v1.4/geocode";
 
